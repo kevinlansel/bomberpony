ef90c93bee5aabfcbb35402209f006d5e0d98d74
wojciak.maxime@gmail.com <wojciak.maxime@gmail.com@aac9e8e4-c511-0dd5-8538-376263b6fa18>
2013-05-13 09:00:15 +0000

    Ajout Bombe

---
+++ Bombe.cpp
@@ -0,0 +1,19 @@
+//
+// Bombe.cpp for Bombe in /home/haz3/projet/bomberpony
+// 
+// Made by Maxime Wojciak
+// Login   <wojcia_m@epitech.net>
+// 
+// Started on  Mon May 13 10:55:07 2013 Maxime Wojciak
+// Last update Mon May 13 10:58:44 2013 Maxime Wojciak
+//
+
+#include	"Bombe.hpp"
+
+Bombe::Bombe() {
+
+}
+
+Bombe::~Bombe() {
+
+}
