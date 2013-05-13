02058ea04de511dd7c1bf4a19698afebfcffa4f5
maxime.sauvage.14@gmail.com <maxime.sauvage.14@gmail.com@aac9e8e4-c511-0dd5-8538-376263b6fa18>
2013-05-13 08:37:53 +0000

    ok

---
+++ trunk/Collision.cpp
@@ -0,0 +1,21 @@
+#include	"Collision.hpp"
+
+Collision::Collision()
+{
+
+}
+
+Collision::~Collision()
+{
+
+}
+
+bool		Collision::checkCollisionWall()
+{
+  // si la position du player == a la position d'un obstacle
+}
+
+bool		Collision::checkCollisionPlayer()
+{
+  // si la position du player == a la position d'un autre player
+}
---
+++ trunk/Collision.hpp
@@ -0,0 +1,13 @@
+#ifndef		__COLLISION_HPP__
+# define	__COLLISION_HPP__
+
+class		Collision
+{
+public:
+  Collision();
+  ~Collision();
+  bool	checkCollisionWall();
+  bool	checkCollisionPlayer();
+}
+
+#endif
