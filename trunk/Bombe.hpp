969e3bb40bab08800d796bb641de75dbb04b7d42
wojciak.maxime@gmail.com <wojciak.maxime@gmail.com@aac9e8e4-c511-0dd5-8538-376263b6fa18>
2013-05-13 09:00:15 +0000

    Ajout Bombe

---
+++ trunk/Bombe.hpp
@@ -0,0 +1,20 @@
+//
+// Bombe.hpp for Bombe in /home/haz3/projet/bomberpony
+// 
+// Made by Maxime Wojciak
+// Login   <wojcia_m@epitech.net>
+// 
+// Started on  Mon May 13 10:55:02 2013 Maxime Wojciak
+// Last update Mon May 13 10:58:46 2013 Maxime Wojciak
+//
+
+#ifndef		_BOMBE__
+#define		_BOMBE__
+
+class Bombe {
+  public:
+
+  private:
+};
+
+#endif /* !_BOMBE__ */
